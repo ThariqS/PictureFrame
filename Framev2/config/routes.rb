a86ec1d640b416fa247fcbe0c92253bfdd58b3e5
ThariqS <trq212@gmail.com>
2010-08-16 13:04:58 -0400

    changed html image error

--- Framev2/config/routes.rb
+++ Framev2/config/routes.rb
@@ -7,7 +7,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
   
   map.resources :clicks
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
